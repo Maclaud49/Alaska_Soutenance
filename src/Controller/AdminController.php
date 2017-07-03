8057f5e541cbd8088427d88f9eadffafe566faac
Maclaud49 <mickael@parlow-co.com>
2017-07-03 16:07:58 +0200

    Integrated CKeditor services Some layout changes on article page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -38,6 +38,7 @@ class AdminController {
      */
     public function addArticleAction(Request $request, Application $app) {
         $article = new Article();
+        $article->setViewsNb(0);
         $article->setLastUpdatedDate(date("Y-m-d H:i:s"));
         $articleForm = $app['form.factory']->create(ArticleType::class, $article);
         $articleForm->handleRequest($request);
