3aafca4ca73dbfccede0dae3690fddacfac37c44
Maclaud49 <mickael@parlow-co.com>
2017-07-17 14:14:57 +0200

    *Corrected data base info

--- db/database.sql
+++ db/database.sql
@@ -1,4 +1,4 @@
-create database if not exists alaska character set utf8 collate utf8_unicode_ci;
-use alaska;
+create database if not exists db688769058 character set utf8 collate utf8_unicode_ci;
+use db688769058;
 
 grant all privileges on db688769058.* to 'dbo688769058'@'localhost' identified by 'Alaska2017&';
